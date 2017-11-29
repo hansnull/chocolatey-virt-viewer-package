67c8652312302f65911d8f53b8451888125755b9
hansnull <t-len@gmx.de>
2017-11-29 10:45:54 +0100

    Installer increased to 6.0.256.

--- virt-viewer/tools/chocolateyinstall.ps1
+++ virt-viewer/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'virt-viewer'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://virt-manager.org/download/sources/virt-viewer/virt-viewer-x86-5.0.msi'
-$url64      = 'https://virt-manager.org/download/sources/virt-viewer/virt-viewer-x64-5.0.msi'
+$url        = 'https://virt-manager.org/download/sources/virt-viewer/virt-viewer-x86-6.0.msi'
+$url64      = 'https://virt-manager.org/download/sources/virt-viewer/virt-viewer-x64-6.0.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -16,9 +16,9 @@ $packageArgs = @{
 
   softwareName  = 'VirtViewer*'
 
-  checksum      = '7bbccb04d8f085b459390321988aae1d'
+  checksum      = 'dfd8b783c4a601f5f612ca9be34c0f29'
   checksumType  = 'md5'
-  checksum64    = 'bfcb8019b96002a499e3ee8c11c1813c'
+  checksum64    = '67062b8ce7519340eb685aa03464a14b'
   checksumType64= 'md5'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
